6f881aa5c2be2688816a7eafa53b92ca850ebd1e
William Redenbaugh <wmredenbaugh@gmail.com>
2023-06-17 03:43:54 -0700

    UDP server HAL generated by the lovely chatgpt

--- chal_struct_definitions.h
+++ chal_struct_definitions.h
@@ -1,8 +1,13 @@
 #ifndef _CHAL_STRUCT_DEFINITIONS_H
 #define _CHAL_STRUCT_DEFINITIONS_H
 
-typedef struct os_udp_server_t{
-   
-}os_udp_server_t;
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+typedef struct {
+    int socket_fd;
+    struct sockaddr_in server_address;
+} os_udp_server_t;
 
 #endif
--- os_wifi.cpp
+++ os_wifi.cpp
@@ -1,5 +1,10 @@
 #include "global_includes.h"
-
+#include<stdio.h>	//printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include <unistd.h>
 
 #define DEBUG_WIFI
 
@@ -10,8 +15,6 @@
 #endif
 
 int os_wifi_connect_sta(char *ssid, char *password) {
- 
-
   return OS_RET_OK;
 }
 
@@ -21,9 +24,32 @@ int os_wifi_disconnect_sta() {
 
 os_udp_server_t *os_wifi_setup_udp_server(int port) {
 
-  os_udp_server_t *udp_server =  new os_udp_server_t; 
+  os_udp_server_t *udp = (os_udp_server_t*)malloc(sizeof(os_udp_server_t));
+  if (udp == NULL) {
+      return NULL;  // Failed to allocate memory
+  }
 
-  return udp_server;
+  // Create socket
+  udp->socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (udp->socket_fd < 0) {
+      free(udp);
+      return NULL;  // Failed to create socket
+  }
+
+  // Set up server address
+  memset(&udp->server_address, 0, sizeof(struct sockaddr_in));
+  udp->server_address.sin_family = AF_INET;
+  udp->server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+  udp->server_address.sin_port = htons(port);
+
+  // Bind socket to server address
+  if (bind(udp->socket_fd, (struct sockaddr *)&udp->server_address, sizeof(struct sockaddr_in)) < 0) {
+      close(udp->socket_fd);
+      free(udp);
+      return NULL;  // Failed to bind socket
+  }
+
+  return udp;
 }
 
 int os_wifi_deconstruct_udp_server(os_udp_server_t *udp){
@@ -31,7 +57,8 @@ int os_wifi_deconstruct_udp_server(os_udp_server_t *udp){
     return OS_RET_NULL_PTR;
   }
 
-  delete udp;
+  close(udp->socket_fd);
+  free(udp);
 
   return OS_RET_OK;
 }
@@ -42,12 +69,33 @@ int os_wifi_start_udp_transmission(os_udp_server_t *udp, char *ip, uint16_t port
     return OS_RET_NULL_PTR;
   }
 
+  if(ip == NULL){
+    return OS_RET_INVALID_PARAM;
+  }
+
+  struct sockaddr_in dest_addr;
+  memset(&dest_addr, 0, sizeof(struct sockaddr_in));
+  dest_addr.sin_family = AF_INET;
+  dest_addr.sin_port = htons(port);
+  if (inet_pton(AF_INET, ip, &(dest_addr.sin_addr)) <= 0) {
+    return OS_RET_INVALID_PARAM;
+  }
+
+  if (connect(udp->socket_fd, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in)) < 0) {
+    return OS_RET_TIMEOUT;
+  }
+
   return OS_RET_OK;
 }
 
 int os_wifi_stop_udp_transmission(os_udp_server_t *udp) {
   if(udp == NULL){
     return OS_RET_NULL_PTR;
+  }
+
+  // Disconnect the socket
+  if (connect(udp->socket_fd, NULL, 0) < 0) {
+    return OS_RET_INT_ERR;
   }
 
   return OS_RET_OK;
@@ -57,24 +105,31 @@ int os_wifi_transmit_udp_packet(os_udp_server_t *udp, uint16_t packet_size, uint
   if(udp == NULL){
     return OS_RET_NULL_PTR;
   }
-
-    return OS_RET_OK;
-}
-
-static inline int read_packet(os_udp_server_t *udp, uint16_t *packet_size, uint8_t *arr){
-  if(udp == NULL){
-    return OS_RET_NULL_PTR;
+  ssize_t bytes_sent = send(udp->socket_fd, arr, packet_size, 0);
+  if (bytes_sent < 0) {
+      return OS_RET_INT_ERR;
   }
 
-  return OS_RET_OK;;
+  return OS_RET_OK;
 }
 
 int os_wifi_receive_packet(os_udp_server_t *udp, uint16_t *packet_size, uint8_t *arr, uint32_t timeout_ms) {
-  if(udp == NULL){
+  if(udp == NULL || packet_size == NULL || arr == NULL){
     return OS_RET_NULL_PTR;
   }
 
-  
+  struct timeval timeout;
+  timeout.tv_sec = timeout_ms / 1000;
+  timeout.tv_usec = (timeout_ms % 1000) * 1000;
+
+  setsockopt(udp->socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout));
+
+  ssize_t bytes_received = recv(udp->socket_fd, arr, *packet_size, 0);
+  if (bytes_received < 0) {
+    return OS_RET_INT_ERR;  // Receiving failed
+  }
+
+  *packet_size = bytes_received;
   return OS_RET_NO_AVAILABLE_DATA;
 }
 
@@ -83,5 +138,17 @@ int os_wifi_receive_packet_indefinite(os_udp_server_t *udp, uint16_t *packet_siz
     return OS_RET_NULL_PTR;
   }
 
-  return OS_RET_OK;
+  struct timeval timeout;
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 0;
+
+  setsockopt(udp->socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout));
+
+  ssize_t bytes_received = recv(udp->socket_fd, arr, *packet_size, 0);
+  if (bytes_received < 0) {
+    return OS_RET_INT_ERR;  // Receiving failed
+  }
+
+  *packet_size = bytes_received;
+  return OS_RET_NO_AVAILABLE_DATA;
 }
